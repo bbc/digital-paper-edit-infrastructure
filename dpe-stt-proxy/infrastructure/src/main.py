dc0526e472e8215fd57b20ce55056892ddc3ea0a
Pietro <pietro.passarelli@bbc.co.uk>
2019-07-08 16:13:17 +0100

    STT Service Cloud formation  (#7)
    * Initial commit
    
    * initial infrastructure folder
    
    changing app name end point to be digital-paper-edit-stt in stacks parameters, dns and main
    
    * Saving intermediate progress
    
    * saving progress
    
    sqs pstt end points
    
    * Updated Queues
    
    * Moving folder to fit with the cosmos project name
    
    * Adding queue parameters
    
    * generate queue stack
    
    * Updating stacks
    
    * Fixed config files
    
    * Updated stacks
    
    * Integer invalid parameter type, number is valid
    
    * Additional config from dotenv

---
+++ dpe-stt-proxy/infrastructure/src/main.py
@@ -0,0 +1,6 @@
+from cosmosTroposphere import CosmosTemplate
+t = CosmosTemplate(description="Digital Paper Edit STT",
+                   component_name="digital-paper-edit-stt",
+                   project_name="news-labs",
+                   )
+print(t.to_json())
