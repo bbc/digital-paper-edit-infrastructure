36cdeea26e192c59f601d8eaf052a013d22f447d
Eimi Okuno <emettely@users.noreply.github.com>
2019-07-10 08:48:12 -0500

    Monitoring (#10)
    * Adding Cloudwatch logging
    
    * Adding policy so that the logging is possible from the instance

---
+++ dpe-stt-proxy/infrastructure/src/monitoring.py
@@ -0,0 +1,29 @@
+from troposphere import Parameter, Ref, Template, Join, GetAtt, Output
+from troposphere.logs import LogGroup, LogStream
+import sys
+
+t = Template()
+
+t.add_version("2010-09-09")
+
+logGroupName = t.add_parameter(Parameter(
+    "LogGroupName",
+    Type="String"
+))
+
+environment = t.add_parameter(Parameter(
+    "Environment",
+    Type="String"
+))
+
+LogGroup = t.add_resource(LogGroup(
+    "LogGroup",
+    LogGroupName=Join("-", [Ref(environment), Ref(logGroupName)]),
+    RetentionInDays=14
+))
+
+template = t.to_json()
+if len(sys.argv) > 1:
+    open(sys.argv[1], "w").write(template + "\n")
+else:
+    print(template)
