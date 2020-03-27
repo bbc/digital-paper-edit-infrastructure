a20e603e12cc07151b427a6aa161f291f1865264
Eimi Okuno <eimi.okuno@bbc.co.uk>
2020-03-27 16:11:51 +0900

    - Removing dpe-api, dpe-client - Removing stacks in dpe-stt-proxy: main, monitoring, s3 buckets - leaving user - Updating policy to point to Ashley's service

--- dpe-client/infrastructure/src/main.py
+++
@@ -1,6 +0,0 @@
-from cosmosTroposphere import CosmosTemplate
-t = CosmosTemplate(description="Digital Paper Edit Infrastructure",
-                   component_name="digital-paper-edit-infrastructure",
-                   project_name="news-labs",
-                   )
-print(t.to_json())
